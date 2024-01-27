8ca41beb603cd803c0e98075c9b04019a516f898
Paul Phatharacharukul <paulppath@Pauls-MacBook-Pro.local>
2024-01-27 09:55:10 -0500

    methods added in BrowserUtil

--- src/test/java/utils/BrowserUtil.java
+++ src/test/java/utils/BrowserUtil.java
@@ -1,10 +1,18 @@
 package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.lang.Thread.sleep;
 
 public class BrowserUtil
 {
@@ -50,4 +58,121 @@ public class BrowserUtil
                 driver.switchTo().window(eachId);
         }
     }
+    public static void highlightElement(WebElement element)
+    {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        for (int i = 0; i < 2; i++)
+        {
+            try {
+                if (i % 2 == 0) {
+                    js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "color: black;" +
+                            "border: 3px solid red; background: yellow");
+                        //TODO: Apply screenshot here
+                } else {
+                    sleep(600);
+                    js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void waitForClickAbility(WebElement element)
+    {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+    public static void waitForElementVisibility(WebElement element){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    public static void sleep (int milliSec)
+    {
+        try{
+            Thread.sleep(milliSec);
+        }catch(InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public static void scrollIntoView(WebElement element)
+    {
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].scrollIntoView(true)", element);
+    }
+
+    public static void sendKeys(WebElement element, String inputText)
+    {
+        // TODO: Apply report -> log info ("Entered the text ", element);
+        waitForClickAbility(element);
+        scrollIntoView(element);
+        element.sendKeys(inputText);
+        highlightElement(element);
+    }
+    public static String getText (WebElement element)
+    {
+        // TODO: Apply report -> logInfo("Retrieved TEXT: ", element);
+        waitForClickAbility(element);
+        scrollIntoView(element);
+        highlightElement(element);
+        return element.getText();
+    }
+    public static void click(WebElement element)
+    {
+        //TODO : Apply report -> logInfo("Clicked on a BUTTON: ", element);
+        waitForClickAbility(element);
+        scrollIntoView(element);
+        highlightElement(element);
+        element.click();
+    }
+    public static void assertEquals(String actual, String expected)
+    {
+        // TODO : Apply report -> logInfo("Expected: " + expected);
+        // TODO : Apply report -> logInfo("Actual: " + actual);
+        Assert.assertEquals(actual, expected);
+    }
+    public static void assertFalse(boolean result)
+    {
+        // TODO : Apply report -> logInfo("Expected: + result);
+        Assert.assertFalse(result);
+    }
+    public static void assertTrue(boolean result)
+    {
+        // TODO : Apply report -> logInfo("Expected: + result);
+        Assert.assertTrue(result);
+    }
+    public static void isDisplayed(WebElement element)
+    {
+        //TODO: Apply report -> logInfo("Displayed at: ", element);
+        waitForElementVisibility(element);
+        scrollIntoView(element);
+        highlightElement(element);
+        Assert.assertTrue(element.isDisplayed());
+    }
+    public static boolean isEnable(WebElement element)
+    {
+        //TODO: Apply report -> logInfo("Is enable at: ", element);
+        waitForElementVisibility(element);
+        scrollIntoView(element);
+        highlightElement(element);
+        return element.isEnabled();
+    }
+    public static boolean isDisable(WebElement element)
+    {
+        //TODO: Apply report -> logInfo("Is disable at: ", element);
+
+        scrollIntoView(element);
+        highlightElement(element);
+        if(element.isEnabled())
+            return false;
+        else
+            return true;
+    }
+    public static void selectByVisibleText(WebElement element, String text)
+    {
+        Select select = new Select(element);
+        select.selectByVisibleText(text);
+    }
+    
+
 }
